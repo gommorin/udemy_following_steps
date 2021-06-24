a8414c98f2babba781a339f8759e16d2bccfc72b
gommorin <gommorin@Luiss-iMac.local>
2021-06-24 17:45:58 -0500

    cambio de color de fondo

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function update() {
   progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
 
   // para habilitar y deshabilitar los botónes según el avance
-  if(currentActive === 1) {
+  if (currentActive === 1) {
     prev.disabled = true
   } else if (currentActive === circles.length) {
     next.disabled = true
